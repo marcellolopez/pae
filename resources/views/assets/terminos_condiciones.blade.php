03271f028d704fbd66cf9ae8b49de3d39bd9501f
Claudio Toro <ctoro@cetep.cl>
2020-04-26 14:44:41 -0400

    Condiciones y terminos

--- resources/views/assets/terminos_condiciones.blade.php
+++ resources/views/assets/terminos_condiciones.blade.php
@@ -31,88 +31,42 @@
           <div class="col-md-12">
 
             <div class="body">
-              <h2 class="text-center"><strong>Consentimiento Informado</strong></h2>
-
-              <span class="subTitleUp">
-                I.  DESCRIPCIÓN DEL SERVICIO
-              </span>
-
-              <p><strong>"Mentalizados en Ti"</strong> es una plataforma que ofrece servicios de “Orientación Telefónica por parte de Psicólogos Expertos” en el contexto de Pandemia de Coronavirus, con el objetivo de facilitar la provisión de servicios a distancia desde el ámbito de la promoción y prevención de Salud Mental.</p>
-
-              <span class="subTitleLow">Consulta Médica de Psiquiatría y Psicología: </span>
-              <p>Los principales objetivos de la Teleconsulta de psiquiatría y psicología son evaluar síntomas actuales, proponer un diagnóstico y establecer plan de tratamiento, mediante estrategias de telemedicina para la entrega de prestaciones de salud mental a distancia. </p>
-              <p>El servicio de teleconsulta no reemplaza a una consulta de urgencia de salud mental.</p>
-              <p>La teleconsulta no sustituye la atención presencial, o, la relación que los pacientes tengan con sus médicos tratantes.</p>
-              <p>El equipo de profesionales se encuentra formado por psiquiatras y psicólogos, con quien acepta iniciar un proceso terapéutico a distancia, cuya duración dependerá de las problemáticas señaladas por el paciente y su gravedad, las sesiones de teleconsulta tendrán una duración de 30 minutos (psiquiatría) o 45 minutos (psicología), con una frecuencia a determinar por el/la psiquiatra/psicóloga, según las necesidades del paciente.</p>
-              <p>Para garantizar la calidad y rapidez de nuestro servicio, debe tener en conocimiento lo siguiente:</p>
+              <p><strong><u>Consentimiento Informado </u></strong></p>
+              <p><strong>&nbsp;</strong></p>
+              <p><strong>&lsquo;&lsquo;Mentalizados en Ti&rsquo;&rsquo; </strong>es una plataforma que ofrece servicios de &ldquo;Orientaci&oacute;n Telef&oacute;nica por parte de Psic&oacute;logos Expertos&rdquo; en el contexto de Pandemia de Coronavirus, con el objetivo de facilitar la provisi&oacute;n de servicios a distancia desde el &aacute;mbito de la promoci&oacute;n y prevenci&oacute;n de Salud Mental.</p>
+              <p>Usted ser&aacute; contactado por el Psic&oacute;logo/a, parte de nuestro equipo de profesionales expertos, con quien acepta realizar una llamada de Orientaci&oacute;n Telef&oacute;nica, cuya duraci&oacute;n depender&aacute; de la naturaleza de las consultas realizadas y nivel de sintomatolog&iacute;a psicol&oacute;gica presentada.</p>
+              <p>El objetivo del servicio de Orientaci&oacute;n Telef&oacute;nica es resolver dudas en caso de presentaci&oacute;n de s&iacute;ntomas, acompa&ntilde;ar, orientar, brindar apoyo psicol&oacute;gico y contenci&oacute;n emocional en casos de crisis.</p>
+              <p>Para garantizar la calidad y rapidez de nuestro servicio, toda persona que demande atenci&oacute;n por esta v&iacute;a debe firmar el presente consentimiento informado y tener conocimiento de lo siguiente:&nbsp;</p>
+              <p>&nbsp;</p>
               <ol>
-                <li><p>En caso de pesquisar durante el tratamiento algún trastorno psiquiátrico de mayor severidad, que requiera de una intervención diferente, el paciente será informado y será derivado a otro profesional de la salud que lo atienda de manera presencial.</p></li>
-                <li><p>Se solicita puntualidad en las citas: el número de médicos y box es limitado, y el atraso en una sesión supondría que otras personas se viesen afectadas.</p></li>
-                <li><p>Se requiere una asistencia continuada a las sesiones, en función de lo que el/la profesional determine, con previo aviso y con la máxima antelación posible en caso de no poder acudir. Una duración excesiva de la intervención debida a la falta de regularidad en la misma afectaría negativamente el proceso terapéutico.</p></li>
-                <li><p>El Centro Médico se reserva la decisión de interrupción de la intervención si se cumplen el punto 1, o se incumplen los puntos 2 o 3.</p></li>
-                <li><p>La entrega de recetas retenidas será entregada de manera diferida para lo cual se darán indicaciones al momento de la consulta. </p></li>
+              <li>El usuario al inicio de la atenci&oacute;n deber&aacute; entregar de manera obligatoria el nombre y el tel&eacute;fono de contacto, de quien ser&aacute; su apoderado en caso de necesitar ayuda de manera urgente. Quien ser&aacute; contactado s&oacute;lo en caso de alguna urgencia o emergencia.</li>
               </ol>
-              <span class="subTitleLow">Consulta Médica electiva y otras profesiones: </span>
-              <p>La teleconsulta es entregada por un médico u otro profesional de la salud, enfocado en atención primaria y orientaciones en salud. En esta modalidad, el profesional atiende al paciente mediante una video llamada, consiguiendo a través de esta plataforma, los antecedentes clínicos del paciente que sean requeridos para dar curso a la atención. La teleconsulta tiene ciertas limitaciones, como el no poder realizar examen físico y no poder emitir algunos documentos como licencia médica. </p>
-              <p>El servicio excluye atenciones de Urgencia o Emergencia médica. Nuestra recomendación es que usted ante una emergencia o urgencia médica no dude en acudir inmediatamente a un establecimiento de salud que pueda otorgar esta prestación. </p>
-              <p>La teleconsulta no reemplaza o sustituye la atención presencial o la relación con su médico y/o equipo médico. </p>
-              <p>La teleconsulta puede generar derivaciones a urgencia o consultas presenciales. </p>
-              <p>Los pacientes atendidos por teleconsulta son los responsables de seguir con las recomendaciones clínicas. </p>
-
-              <span class="subTitleLow">• Receta Médica:</span>
-              <p>En caso de que el criterio médico lo permita, al finalizar la atención, el paciente recibirá de parte del médico una receta médica simple, para que pueda adquirir los medicamentos que requiera para el tratamiento indicado por el médico. </p>
-              <p>Excluye recetas retenidas y recetas cheque. </p>
-
-              <span class="subTitleLow">• Certificados Médicos y licencias médicas: </span>
-              <p>El médico podrá entregar al paciente certificados de atención médica, válido como justificativo para ser presentado en la Institución correspondiente. También podrá entregar certificados de derivaciones médicas.
-                Se excluye la emisión de licencias médicas digitales a distancia.
-              </p>
-              <p>El médico podrá entregar Licencias Médicas impresas o digitales de manera presencial en Centro Médico…. </p>
-
-              <span class="subTitleLow">• Órdenes de examen: </span>
-              <p>El médico podrá emitir órdenes de examen, que permitan verificar, identificar o descartar un determinado diagnóstico. </p>
-
-              <span class="subTitleLow">Exclusiones:</span>
-              <p>La atención clínica online no incluye toda otra actividad clínica no mencionada previamente.</p>
-
-              <span class="subTitleUp">II.  PROCEDIMIENTO PARA SOLICITAR ATENCIÓN POR TELECETEP: </span>
-              <ol>
-                <li><p>El paciente debe ingresar a http://telecetep.cl/.</p></li>
-                <li><p>Deberá seleccionar profesional o especialidad y luego día y hora, según disponibilidad que se encuentre en ese momento. </p></li>
-
-                <li><p>Una vez confirmada la reserva se deberá ejecutar el pago siguiendo los pasos que se dispondrán para ello. </p></li>
-                <li><p>Una vez finalizada la etapa de ingreso y pago de la atención requerida, el profesional atenderá la consulta a través de una video llamada, y en el caso correspondiente, enviará a su correo electrónico recetas médicas, ordenes de exámenes, certificados médicos, entre otros. </p></li>
-                <li><p>El médico podrá enviar los documentos clínicos necesarios, en formato PDF, al correo electrónico registrado. </p></li>
-                <li><p>La reserva de horas estará disponible 365 días del año, pero la oferta disponible será restringida a la capacidad de los profesionales y la agenda que tengan disponible para este servicio. </p></li>
-                <li><p>En caso de que la persona que requiera atención sea un menor de 18 años o por su condición de salud requiera actuar debidamente representado, deberá estar acompañado de una persona mayor de edad a cuyo cuidado se encuentre de su representante legal durante toda la atención clínica. </p></li>
-                <li><p>Para asegurar la calidad de la video llamada y una buena experiencia de atención del paciente, éste debe estar conectado a una conexión estable de internet. Se recomienda usar una conexión a una red WiFi, o también red móvil 4G. La velocidad mínima de conexión es de 400 kbps. </p></li>
+              <p>&nbsp;</p>
+              <ol start="2">
+              <li>En caso de pesquisar durante la atenci&oacute;n alg&uacute;n s&iacute;ntoma de mayor gravedad, que requiera de una intervenci&oacute;n diferente, el paciente ser&aacute; informado y ser&aacute; derivado a otro profesional de la salud para ser atendido de manera presencial o a trav&eacute;s de Teleconsulta.</li>
               </ol>
-
-              <span class="subTitleUp">III. VALOR DEL SERVICIO DE TELECONSULTA</span>
-              <p>El valor del servicio de atención clínica online deberá ser entregado antes de recibir la atención, pudiendo utilizar las opciones de pago que estarán disponibles una vez realizado el agendamiento de la consulta. Las formas de pago podrán ser, pero no se limitarán a: webpay y otras tecnologías de pago virtual. </p>
-              <p>La cobertura de teleconsulta depende de la especialidad, de acuerdo con las normativas vigentes.
-                Centro Médico Cetep se reserva el derecho de modificar los precios de teleconsulta cuando estime pertinente.
-              </p>
-
-              <span class="subTitleUp">IV. REGISTRO Y SEGURIDAD </span>
-              <p>Con el fin de acceder al sitio y al servicio de teleconsulta, usted garantiza que tiene al menos dieciocho años y que posee el derecho legal y la capacidad, en nombre propio o de un menor de edad del cual usted es su representante legal, de acuerdo con las presentes Condiciones de Uso. </p>
-              <p>Regístrese para acceder al servicio de atención clínica online bajo su propio nombre y use este servicio de conformidad con las Condiciones de Uso y cumpla con las obligaciones del presente documento. Usted acepta plenamente, con exactitud y veracidad crear su reserva de cita clínica online con datos verdaderos y fidedignos, incluyendo su nombre completo, dirección, ciudad, comuna, correo, fecha de nacimiento, número de teléfono fijo o móvil, correo electrónico y cualquier otro dato que sea requerido para la correcta cita clínica. </p>
-              <p>Usted se compromete a prohibir cualquier otro uso de su identificación en el sitio y se compromete a notificar inmediatamente a Centro Médico Cetep de cualquier irregularidad, uso no autorizado o mal uso de esta o de cualquier problema de seguridad, que usted detecte en el uso de su identificación en este sitio. </p>
-              <p>Con el fin de determinar el cumplimiento de estas condiciones de uso, nos reservamos el derecho, pero no la obligación, de controlar el acceso y el uso del sitio y del servicio de atención clínica online y a no proporcionar la atención por el mal uso real o potencial de este servicio, o por el incumplimiento de los términos y condiciones de uso del sitio y de este servicio. </p>
-
-              <span class="subTitleUp">V. PREVENCIÓN DE FRAUDE Y SEGURIDAD </span>
-              <p>Centro Médico Cetep puede ponerse en contacto con usted por teléfono o correo electrónico para verificar su información de la cita creada y usted se compromete a facilitar dicha información para asegurarse de que no haya creado fraudulentamente su cita clínica. Si usted no proporciona esta información solicitada dentro de 48 horas, Centro Médico Cetep por su seguridad se reserva el derecho de suspender, interrumpir o negar su acceso y uso del sitio y el servicio, hasta que la información sea proporcionada por usted. </p>
-              <p>El profesional a cargo de su atención podría solicitarle al principio de esta verificar su identidad mediante su cedula de identidad nacional.</p>
-              <p>Centro Medico Cetep no solicita datos personales o bancarios por teléfono o mail, solo información relacionada con su atención médica.</p>
-
-              <span class="subTitleUp">VI. ANTECEDENTES PERSONALES DE LOS PACIENTES </span>
-              <p>Los datos personales de los usuarios recibidos y almacenados por Centro Médico Cetep con ocasión de este servicio, son confidenciales y serán usados y/o tratados en conformidad a las disposiciones legales sobre el tema, especialmente lo dispuesto por la Ley N.º 19.628 Sobre Protección de Datos de Carácter Personal, y la Ley N.º 20.584 que Regula los Derechos y Deberes que tienen las Personas en relación con Acciones Vinculadas a su Atención de Salud. </p>
-              <p>Los datos personales y sensibles que el paciente proporcione para acceder a este servicio y durante el otorgamiento de las prestaciones que requiera en el contexto de la atención clínica online, son verdaderos, y es en ellos donde se fundamenta la atención que recibe por parte de los profesionales médicos de Centro Médico Cetep, obligándose el paciente o su representante a mantener actualizada dicha información. </p>
-              <p>Toda información, declaración falsa u omisión por parte del paciente o su representante en cuanto a lo anterior, habilitará a Centro Médico Cetep a cancelar la cita de teleconsulta. </p>
-
-              <span class="subTitleUp">VII. RESERVA DE DERECHOS </span>
-              <p>Centro Médico Cetep se reserva el derecho de no otorgar el servicio de atención médica online por mal uso real o potencial de este servicio, ni en caso de fuerza mayor o caso fortuito, catástrofe u ocurrencia en el momento de solicitar la atención de algún acto terrorista u otro similar. </p>
-              <p>Centro Médico Cetep no se responsabiliza de los errores y omisiones propias del paciente o representante legal, o por el hecho o causa fortuita, por actos u omisiones de terceros.</p>
+              <p>&nbsp;</p>
+              <ol start="3">
+              <li>Es imprescindible el cumplimiento de las tareas y actividades que &eacute;l/la profesional sugiera y estime oportunas.</li>
+              </ol>
+              <p>&nbsp;</p>
+              <ol start="4">
+              <li>Este servicio garantiza la confidencialidad de la informaci&oacute;n entregada, a excepci&oacute;n de aquellos casos de riesgo personal o a terceros (por expresi&oacute;n manifiesta de la persona atendida o por valoraci&oacute;n del/de la terapeuta), en cumplimiento con el Art&iacute;culo 38 del C&oacute;digo de &Eacute;tica del Colegio M&eacute;dico de Chile. Se obliga a que la informaci&oacute;n entregada por el paciente en el contexto de la atenci&oacute;n se realice de forma segura resguardando su privacidad.&nbsp;</li>
+              </ol>
+              <p>&nbsp;</p>
+              <ol start="5">
+              <li>Cetep eclara contar con los procedimientos internos que cumplen con los est&aacute;ndares dispuestos en la Ley N&ordm; 19.628 sobre Protecci&oacute;n de la Vida Privada y dem&aacute;s normativa aplicable, con el fin de salvaguardar el car&aacute;cter reservado de la Informaci&oacute;n Confidencial, e impedir que la misma sea dada a conocer en forma intencional o negligente.</li>
+              </ol>
+              <p>&nbsp;</p>
+              <ol start="6">
+              <li>El servicio de &ldquo;Orientaci&oacute;n Telef&oacute;nica de Psic&oacute;logo Experto&rdquo; no ser&aacute; grabado para resguardar la confidencialidad y privacidad tanto del paciente, como de la informaci&oacute;n revelada.</li>
+              </ol>
+              <p>&nbsp;</p>
+              <p>Nuestro prop&oacute;sito es prestar un servicio serio, eficaz y &uacute;til. Le rogamos que contribuya con su colaboraci&oacute;n a lograr este objetivo.&nbsp;</p>
+              <p>&nbsp;</p>
+              <p><strong>Si usted est&aacute; de acuerdo con el presente consentimiento, por favor completar con sus datos a continuaci&oacute;n y enviar documento v&iacute;a email a </strong><a href="mailto:met@cetep.cl"><strong>met@cetep.cl</strong></a><strong> .</strong></p>
+              <p>&nbsp;</p>
+              <p>Muchas gracias.&nbsp;</p>
             </div>
           </div>
         </div>
