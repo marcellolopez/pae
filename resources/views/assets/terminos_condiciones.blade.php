9251192faf12e14431fcf05ea19d6aa24d76c75e
Claudio Toro <ctoro@cetep.cl>
2020-04-26 17:59:15 -0400

    Terminos

--- resources/views/assets/terminos_condiciones.blade.php
+++ resources/views/assets/terminos_condiciones.blade.php
@@ -21,7 +21,7 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Términos y condiciones del servicio de Teleconsulta</h5>
+        <h5 class="modal-title">Términos y condiciones del servicio</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
